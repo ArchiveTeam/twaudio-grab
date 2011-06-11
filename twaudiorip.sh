018d30a36683a384408f5e320135590c52ea5054
Alard <alard@example.com>
2011-06-11 10:53:44 +0200

    Initial commit of twaudiorip.sh

---
+++ twaudiorip.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# Version 1.
+#
+# Download the newest mp3 files from a Twaud.io user.
+# (Only those mp3's that are listed in the RSS feed.)
+#
+# Usage:  ./twaudiorip.sh ${USERNAME}
+#
+
+USERNAME=$1
+
+# USER_AGENT="FeedFetcher-Google; (+http://www.google.com/feedfetcher.html)"
+USER_AGENT="Googlebot/2.1 (+http://www.googlebot.com/bot.html)"
+
+DATADIR=data/${USERNAME:0:1}/${USERNAME:0:2}/${USERNAME:0:3}/${USERNAME:0:4}/${USERNAME}
+
+START=$(date +%s)
+
+# incomplete result from a previous run?
+if [ -f $DATADIR/.incomplete ]
+then
+  echo "Deleting incomplete download of $USERNAME..."
+  rm -rf $DATADIR
+fi
+
+if [ -d $DATADIR ]
+then
+  echo "$USERNAME already downloaded."
+  exit 2
+fi
+
+echo "Downloading ${USERNAME}:"
+
+# create directory
+mkdir -p $DATADIR
+touch $DATADIR/.incomplete
+
+# touch $DATADIR/.incomplete
+
+# grab profile page
+wget -nv -a "$DATADIR/wget.log" -nc -U "$USER_AGENT" -O $DATADIR/index.html "http://twaud.io/users/${USERNAME}"
+
+# grab rss feed
+wget -nv -a "$DATADIR/wget.log" -nc -U "$USER_AGENT" -O $DATADIR/rss.xml "http://twaud.io/users/${USERNAME}.xml"
+
+# find urls
+# grep output: first line contains audio id,  http://twaud.io/audio/$ID
+#              second line contains s3 url of mp3
+audio_files=`grep -o -E 'http://twaud.io/audio/[^<]+|http://s3[^"]+' $DATADIR/rss.xml`
+
+# parse
+for line in $audio_files
+do
+  if [[ $line =~ twaud.io/audio/ ]]
+  then
+    # extract clip id from url (http://twaud.io/audio/$ID)
+    cur_clip_id=${line:22}
+
+  else
+    if [[ $line =~ s3.amazonaws.com ]]
+    then
+      echo " - clip ${cur_clip_id}"
+      # download clip
+      url=${line//&amp;/&}
+      wget -nv -a "$DATADIR/wget.log" -nc -U "$USER_AGENT" -O $DATADIR/${cur_clip_id}.mp3 "$url"
+      if [[ $? -ne 0 ]]
+      then
+        echo "ERROR: wget returned a non-zero exit code! See $DATADIR/wget.log"
+      fi
+
+      # download html
+      wget -nv -a "$DATADIR/wget.log" -nc -U "$USER_AGENT" -O $DATADIR/${cur_clip_id}.html "http://twaud.io/${cur_clip_id}"
+    fi
+  fi
+done
+
+rm $DATADIR/.incomplete
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+
+echo " Done. ($DIFF seconds)"
+
+
